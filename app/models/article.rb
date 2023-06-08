e6989ed5c0ac6f92d7d629edee1597a4ae30b4ca
Luis Ruiz <l.ruiz5@live.com>
2023-06-08 09:38:53 -0700

    add CRU ops of CRUD

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4, maximum: 100}
-  validates :content, presence: true, length: { minimum: 12, maximum: 500}
+  validates :content, presence: true, length: { minimum: 4, maximum: 500}
 end
